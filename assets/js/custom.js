91c24df8d960c1c62e7f061210913c17fa48cb48
Durgesh <dsable@k12.com>
2023-09-15 12:37:55 +0530

    Fix Responsive issue

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
   });
    // Toggle the mobile menu when the menu button is clicked
    $(".mobile-menu").click(function () {
-    $(".right-section").slideToggle();
+    $("#responsive-nav").slideToggle();
   });
   
   $("#advanced-filters").click(function(){
